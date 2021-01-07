59e17c07cb852a36a271eff52b1f9a17d6eb2f93
Thiago De Morais <demoraisthiago63@gmail.com>
2021-01-07 15:59:40 -0300

    Video player botao de player/pause, stop, aumento e decremento de velocidade de reproducao, avanco e retrocesso, div de controle aparecendo e sumindo de acordo com a posicao do mouse

---
+++ scripts/script.js
@@ -0,0 +1,59 @@
+let video = document.getElementById("video1");
+let flag = 0;
+
+function play(img){
+    if(flag==0){
+        video.play();
+        flag = 1;
+        document.getElementById("playPause").src = "../resources/pause.png";
+    }else{
+        video.pause();
+        flag = 0;
+        document.getElementById("playPause").src = "../resources/play.png";
+
+    }
+}
+
+function stop(){
+    video.pause();
+    video.currentTime = 0 ;
+    video.playbackRate = 1;
+
+}
+
+function decrease(){
+video.playbackRate *= 0.95;
+}
+
+function increase(){
+    video.playbackRate *= 1.05;
+}
+
+function jump_back(){
+    video.currentTime -= 5;
+}
+
+function jump_forward(){
+    video.currentTime += 5
+}
+
+
+function OntoVideo(style){
+    console.log(style);
+    style.opacity = 100 + "%";
+}
+
+function OutVideo(style){
+    console.log(style);
+    console.log(style.opacity);
+    style.opacity = 0 + "%";
+}
+
+
+function IncreaseOpacityContainer(){
+
+}
+
+function DecreaseOpacityContainer(){
+
+}
